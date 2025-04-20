2404e0160f594701468aecb3a51255a37257abcd
Phillip Stiby <phill@stiby.co.uk>
2025-04-20 12:11:26 +0100

    Initial NeoVIM setup for tablet

---
+++ lua/stiby/init.lua
@@ -0,0 +1,2 @@
+require("stiby.remap")
+require("stiby.set")
---
+++ lua/stiby/packer.lua
@@ -0,0 +1,34 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- lua/plugins/rose-pine.lua
+  use {
+	  "rose-pine/neovim",
+	  name = "rose-pine",
+	  config = function()
+		  vim.cmd("colorscheme rose-pine")
+	  end
+  }
+
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use ('nvim-treesitter/playground')
+  use ('theprimeagen/harpoon')
+  use ('mbbill/undotree')
+  use ('tpope/vim-fugitive')
+
+  use({'neovim/nvim-lspconfig'})
+  use({'hrsh7th/nvim-cmp'})
+  use({'hrsh7th/cmp-nvim-lsp'})
+  use({'williamboman/mason.nvim'})
+  use({'williamboman/mason-lspconfig.nvim'})
+
+end)
